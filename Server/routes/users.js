fb9cdd1dc517e2c5bb004b4f6e6d70b6c86bd632
여상원 <superkid19962naver.com>
2019-08-03 13:33:11 +0900

    Update users.js

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -5,7 +5,7 @@ var db_user = require('../public/SQL/user_sql')();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('유저');
+  res.send('유저123');
 });
 
 router.get('/name',function(req,res,next){
