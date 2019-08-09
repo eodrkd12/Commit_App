416d2952e526e6f6679cf85a1bfc58c2791f6c61
여상원 <superkid19962naver.com>
2019-08-09 17:23:03 +0900

    add join_room

--- Server/public/SQL/join_room_sql.js
+++ Server/public/SQL/join_room_sql.js
@@ -12,7 +12,42 @@ module.exports=function(){
                 })
             })
         },
+
+        get_join_room_study: function(callback){
+            pool.getConnection(function(err,con){
+                var sql=`select * from join_room where category='study'`;
+                con.query(sql,function(err,result,field){
+                    con.release();
+                    if(err)return;
+                    callback(null,result);
+                })
+            })
+        },
         
+
+        get_join_room_market: function(callback){
+            pool.getConnection(function(err,con){
+                var sql=`select * from join_room where category='market'`;
+                con.query(sql,function(err,result,field){
+                    con.release();
+                    if(err)return;
+                    callback(null,result);
+                })
+            })
+        },
+////////////////////////////////////////////////////////////////
+
+        create_join_room: function(category, number){
+            pool.getConnection(function(err,con){
+                var sql=`insert into join_room(category,room_num) value('${category}',${room_num})`;
+                con.query(sql,function(err,result,field){
+                    con.release();
+                    if(err) console.log(err);
+                    else console.log('채팅방 생성');
+                })
+            })
+        },
+
         pool:pool
     }
 }
--- Server/routes/join_room.js
+++ Server/routes/join_room.js
@@ -12,3 +12,28 @@ router.get('/date',function(req,res,next){
         }
     })
 })
+
+router.get('/study',function(req,res,next){
+    db_join_room.get_join_room_study(function(err,result){
+        if(err) console.log(err);
+        else {
+            console.log("스터디 조회 성공");
+            res.send(result);
+        }
+    })
+})
+
+router.get('/market',function(req,res,next){
+    db_join_room.get_join_room_study(function(err,result){
+        if(err) console.log(err);
+        else {
+            console.log("장터 조회 성공");
+            res.send(result);
+        }
+    })
+})
+///////////////////////////////////////// 
+router.post('/',function(req,res,next){//채팅방 생성
+    db_post.create_join_room(req.category,req.number)
+    res.send('success_chat')
+})
