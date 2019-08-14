9a52135714bbf84892c79e485b051731aa9c1967
eodrkd12 <eodrkd12@gmail.com>
2019-08-15 00:39:21 +0900

    App coding

--- Application/app/src/main/java/com/example/commit/Intro/LoginActivity.kt
+++ Application/app/src/main/java/com/example/commit/Intro/LoginActivity.kt
@@ -11,9 +11,25 @@ package com.example.commit.Intro
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.example.commit.R
+import com.example.commit.Singleton.VolleyService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btn_login.setOnClickListener  {
+            VolleyService.loginReq(this){success ->
+                if(success){
+                    Toast.makeText(this,"통신 성공",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this,"통신 실패",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
